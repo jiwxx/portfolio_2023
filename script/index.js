63d35d8c702d1a31882af54b755fcc84ccbd9b83
jiwoo <wldnslzhf@gmail.com>
2023-09-22 13:46:52 +0900

    스마일, 클라우드 수정 필요

--- script/index.js
+++ script/index.js
@@ -1,10 +1,24 @@
 const smile = document.querySelectorAll('header > nav > .gnb > a > .smile')
 const title = document.querySelectorAll('header > nav > .gnb > a > .title')
-console.log (smile,title)
+const cloud = document.querySelectorAll('main > .cloud > a')
 
-for(let i of smile){i.style.display='none'}
+console.log (smile,title,cloud)
+
+// for(let i of smile){i.style.display='none'}
 title.forEach(function(t,i){
     t.addEventListener('click',function(){
+        for(let j of smile){j.classList.remove('active')}
         smile[i].classList.toggle('active')
     })
+})
+
+// $(window).scroll(function(){
+//     var height = $(window).scrollTop();
+//     console.log(height)
+// })
+window.addEventListener('scroll',function(){
+    console.log(window.pageYOffset)
+    if(this.window.pageYOffset > 0){
+        for(let i of cloud){i.style.animation = 'move 0.8s linear alternate infinite'}
+    }
 })
